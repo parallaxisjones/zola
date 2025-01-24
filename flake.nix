0a28e0ad53854ae76489b3c7e40775fb456bec93
Parker Jones <parker.jones@rithum.com>
2025-01-24 10:29:43 -0500

    some progress

--- flake.nix
+++ flake.nix
@@ -3,42 +3,41 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
-    # You can specify a specific branch or commit if needed
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, ... }:
-    let
-      system = builtins.currentSystem;
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      packages = {
-        "${system}" = pkgs.rustPlatform.buildRustPackage {
-          pname = "myZola";
-          version = "custom";
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages = {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "myZola";
+            version = "custom";
 
-          src = self;
+            src = self;
 
-          cargoSha256 = "0000000000000000000000000000000000000000000000000000000000000000";  # Replace after first build
+            cargoSha256 = "Bp/rPpuV7vZrqoAgvQ5+UptT2OdABkhaiTuUAjfFofY=";
 
-          nativeBuildInputs = [
-            pkgs.openssl,
-            pkgs.pkgconfig,
-            pkgs.libgit2
-            # Add other dependencies as required by Zola
-          ];
+            nativeBuildInputs = [
+              pkgs.openssl
+              pkgs.pkg-config
+              pkgs.libgit2
+              # Add other dependencies as required by Zola
+            ];
 
-          # You might need to specify features or other build options
-          # buildRustFlags = [ "--features" "foo" ];
-
-          meta = with pkgs.lib; {
-            description = "Your custom version of Zola";
-            homepage = "https://www.getzola.org/";
-            license = licenses.mit;
-            # maintainers = with maintainers; [ yourGithubUsername ];
-            platforms = platforms.all;
+            meta = with pkgs.lib; {
+              description = "Your custom version of Zola";
+              homepage = "https://www.getzola.org/";
+              license = pkgs.lib.licenses.mit;
+              # maintainers = with maintainers; [ yourGithubUsername ];
+              platforms = pkgs.lib.platforms.all;
+            };
           };
         };
-      };
-    }
+      }
+    );
 }
+
